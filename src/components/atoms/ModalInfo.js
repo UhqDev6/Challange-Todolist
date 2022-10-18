2578daf5a70385ecbc3284ff4f5f91f97e8c9d13
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2022-10-19 00:09:01 +0800

    updating

--- src/components/atoms/ModalInfo.js
+++ src/components/atoms/ModalInfo.js
@@ -2,7 +2,7 @@ import ICInfo from '../../assets/icons/modal-information-icon.png';
 const ModalInfo = ({setModalInformation}) => {
     return(
         <>
-            <div className="fixed md:inset-0 bg-gray-600 bg-opacity-75">
+            <div className="fixed md:inset-0 bg-gray-600 bg-opacity-75" onClick={() => setModalInformation(false)}>
                 <div className="w-2/6 md:h-auto mx-auto mt-80">
                     <div  data-cy='modal-information' className="bg-white rounded-2xl shadow dark:bg-gray-700">
                         <div className=" flex p-6 justify-between">
