16397d6776b57c20ccfb6dcaf4c3320035d41ea7
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2022-10-19 00:43:23 +0800

    updating

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,7 +1,7 @@
 const Button = ({ title, type, value, icon, background, className='', children, text,  ...props }) => {
     return(
         <>
-            <button {...props} className={`${className} [&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 text-white px-4 py-4 rounded-full`}>
+            <button className={`${className} [&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 text-white px-4 py-4 rounded-full`}  {...props}>
                 {text || children}
             </button>
         </>
