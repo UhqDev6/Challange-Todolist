2841db9b2fccc61175f3c91f951a93465b189f2c
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2022-10-19 12:04:21 +0800

    updating

--- src/components/atoms/ModalInfo.js
+++ src/components/atoms/ModalInfo.js
@@ -3,14 +3,12 @@ const ModalInfo = ({setModalInformation}) => {
     return(
         <>
             <div className="fixed md:inset-0 bg-gray-600 bg-opacity-75" onClick={() => setModalInformation(false)}>
-                <div className="w-2/6 md:h-auto mx-auto mt-80">
-                    <div data-cy='modal-information' className="bg-white rounded-2xl shadow dark:bg-gray-700">
+                    <div data-cy='modal-information' className="bg-white rounded-2xl shadow dark:bg-gray-700 w-2/6 md:h-auto mx-auto mt-80">
                         <div className=" flex p-6 justify-between">
                             <img data-cy='modal-information-icon' src={ICInfo} alt='modal information icon' className='h-6 w-6' />
                             <p data-cy='modal-information-title' className='mr-48'>Activity berhasil dihapus</p>
                         </div>
                     </div>
-                </div>
             </div>
         </>
     );
